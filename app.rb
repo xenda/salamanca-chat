3bb61733d760151fe24956572e1d943d414b2d87
HPNeo <hpneo@hotmail.com>
2013-04-12 15:25:25 -0500

    List chat messages

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+require 'mysql2'
 
 get '/chat_messages' do
   client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'salamanca_production', password: '')
@@ -10,15 +11,17 @@ get '/chat_messages' do
 
   results = client.query("SELECT * FROM comments WHERE comment_type = 'chat' AND video_id = #{video_id} AND (status != 'spam') AND (user_id != #{user_id}) AND (created_at >= '#{since}') ORDER BY created_at DESC")
 
-  user_ids = results.map{ |row| row["user_id"] }.uniq.join(', ')
+  user_ids = results.map{ |row| row["user_id"] }.uniq
 
-  users = client.query("SELECT id, avatar_file_name, first_name FROM users WHERE id IN (#{user_ids})").group_by{ |row| row["id"] }
+  if user_id.count > 0
+    users = client.query("SELECT id, avatar_file_name, first_name FROM users WHERE id IN (#{user_ids.join(', ')})").group_by{ |row| row["id"] }
 
-  results.each do |row|
-    row["user"] = users[row["user_id"]].try(:first)
+    results.each do |row|
+      row["user"] = users[row["user_id"]].try(:first)
 
-    if row["user"]
-      row["user"]["avatar"] = row["user"]["avatar_file_name"].nil? ? "https://graph.facebook.com/#{row["user"]["uid"]}/picture" : row["user"]["avatar_file_name"]
+      if row["user"]
+        row["user"]["avatar"] = row["user"]["avatar_file_name"].nil? ? "https://graph.facebook.com/#{row["user"]["uid"]}/picture" : row["user"]["avatar_file_name"]
+      end
     end
   end
 
