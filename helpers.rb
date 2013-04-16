3e70ee8bf2272cc05ce31ddfb49e80b9e5b30d3e
HPNeo <hpneo@hotmail.com>
2013-04-16 11:36:11 -0500

    Add encoding to helpers.rb

--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 def distance_of_time_in_words(from_time, to_time = Time.now, include_seconds = false, options = {})
   from_time = from_time.to_time if from_time.respond_to?(:to_time)
   to_time = to_time.to_time if to_time.respond_to?(:to_time)
