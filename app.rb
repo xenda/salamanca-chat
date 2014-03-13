92a3b802721b6bb64de8c9f8eab4fe62d4d91f91
Xenda <xendaccount@xendacentral.com>
2014-03-12 23:25:01 -0500

    Changes bd

--- app.rb
+++ app.rb
@@ -30,9 +30,9 @@ class Application < Sinatra::Base
   end
 
   set :database, {
-    host: 'database1.chnoufcqts48.us-west-2.rds.amazonaws.com',
+    host: 'database.chnoufcqts48.us-west-2.rds.amazonaws.com',
     username: 'root',
-    database: 'stoptv_productio',
+    database: 'stoptv_production',
     password: 'stoptvSTOPTV2013'
   }
 
